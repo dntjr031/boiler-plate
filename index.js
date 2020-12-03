a89bfc3c650acdf9a9d58d90b65516f494de2e1a
dntjr031 <63115449+dntjr031@users.noreply.github.com>
2020-12-04 00:25:01 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
+const config = require('./config/key')
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://lama:lama@boilercluster.z8vti.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -14,6 +20,20 @@ mongoose.connect('mongodb+srv://lama:lama@boilercluster.z8vti.mongodb.net/test?r
 
 app.get('/', (req, res) => {
   res.send('Hello World!~~ 안녕하세요!')
+})
+
+app.post('/register', (req, res) => {
+  //회원 가입 할때 필요한 정보들을  client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.  
+
+  const user = new User(req.body)
+    console.log(user.name)
+  user.save((err, userInfo) => {
+      if(err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+  });
 })
 
 app.listen(port, () => {
